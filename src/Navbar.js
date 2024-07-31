3cda8cef31852e17fe3c7a22add715ca59f8f727
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-07-31 22:47:47 +0100

    inline styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
         <h1> The Emma Blog</h1>
         <div className="links">
             <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            <a href="/create" style = {{
+                color: "white",
+                backgroundColor: "#F1356d",
+                borderRadius: '8px'
+            }}>New Blog</a>
         </div>
     </nav>
   );
