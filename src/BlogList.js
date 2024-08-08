6fb2dc6110f7f3975665494e6c660e8a1e4b742c
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-08 15:45:10 +0100

    reusuing function

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = ({blogs, title}) => {
+
+
+  return (
+    <div className="home">
+      <h2>{ title }</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={ blog.id }>
+          <h2>{ blog.title }</h2>
+          <p>writtten by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,18 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
     const [blogs, setBlog] = useState([
       { title: 'My old website', body: 'i want to change my website', author: 'emma', id: 1 },
-      { title: 'My new website', body: 'this is better than my new website', author: 'saint', id: 2 },
-      { title: 'My current website', body: 'i like this new website', author: 'buike', id: 3 }
+      { title: 'what i will eat today', body: ' will eat rice, egg, meat', author: 'saint', id: 2 },
+      { title: 'what workout will i do', body: 'reps of bench, pullups and sit ups', author: 'saint', id: 3 }
     ]);
+
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={ blog.id }>
-          <h2>{ blog.title }</h2>
-          <p>writtten by { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs"/>
     </div>
   )
 }
