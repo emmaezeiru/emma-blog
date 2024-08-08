ffa9cfc3f0a30c1828bff4e5aeaa24165c20ac95
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-08 19:03:16 +0100

    fetching data in useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 
 const Home = () => {
-    const [blogs, setBlog] = useState([
-      { title: 'My old website', body: 'i want to change my website', author: 'emma', id: 1 },
-      { title: 'what i will eat today', body: ' will eat rice, egg, meat', author: 'saint', id: 2 },
-      { title: 'what workout will i do', body: 'reps of bench, pullups and sit ups', author: 'saint', id: 3 }
-    ]);
+    const [blogs, setBlog] = useState(null);
 
-    const [house, setHouse] = useState('bungalow')
-
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter((blog)=> blog.id !== id);
-      setBlog(newBlogs);
-    }
+    useEffect(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setBlog(data)
+        }) 
+    }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={()=> setHouse('duplex')} >Change house</button>
-      <p>{ house }</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   )
 }
