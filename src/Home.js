be1e9a14493b617421b9ab8eac7dfbd399b04e47
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2024-08-12 16:03:38 +0100

    fetch api and handling errors

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,33 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlog] = useState(null);
+    const [isLoading, setisLoading] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          if (!res.ok){
+            throw Error ('there is an error with the data');
+          }
           return res.json();
         })
         .then(data => {
           setBlog(data)
-        }) 
+          setisLoading(false)
+          setError(false)
+        })
+        .catch(err => {
+          console.log(err.message);
+          setError(err.message)
+          setisLoading(false)
+        })
     }, [])
 
   return (
     <div className="home">
+      {error && <div>{ error }</div>}
+      {isLoading && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   )
